b068ab37468933374fe41dc141f132289ef54a1d
Abdurahmon <bekzotovich12@gmail.com>
2023-07-13 18:22:51 +0500

    fixed bugs

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header = (props) => {
       <header className='py-8'>
         <div className='container mx-auto'>
           <div className='flex justify-between items-center'>
-            <span>.bekzotovich12</span>
+            <span>bekzotovich</span>
             <a href={linkBuy} className='btn btn-sm flex items-center justify-center font-secondary' target={blank} style={{backgroundColor: `${props.color}`, borderRadius: `${props.borderRadius}px`}}><BiCoffeeTogo className='mx-1' /> buy me a coffee </a>
           </div>
         </div>
