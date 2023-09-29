4c88c17867c6012494598b49cb56181debf07638
Abdurahmon <bekzotovich12@gmail.com>
2023-09-29 10:39:08 +0500

    Updated

--- src/components/Services.js
+++ src/components/Services.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BsArrowUpRight } from "react-icons/bs";
 import { motion } from "framer-motion";
 import { fadeIn } from "../variants";
+import Image from "../assets/avatar7.jpg";
 
 const services = [
   {
@@ -31,39 +32,16 @@ const Services = (props) => {
         <div className="container mx-auto">
           <div className="flex  flex-col lg:flex-row">
             {/* text */}
-            <motion.div
-              variants={fadeIn("right", 0.3)}
-              initial="hidden"
-              whileInView={"show"}
-              viewport={{ once: false, amount: 0.3 }}
-              className="flex-1 lg:bg-services btn lg:bg-bottom bg-no-repeat mix-blend-normal rounded-xl p-10 mr-5 mb-12 lg:mb-0"
-            >
-              
-              <h2 className="h2 mb-6" style={{ color: `${props.color}` }}>
-                What I Do.{" "}
-              </h2>
-              <h3 className="h3 max-w-[455px] mb-6">
-                I'm a Frontend Developer with over 1 year of experience.
-              </h3>
-              <button
-                className="btn btn-sm z-50"
-                style={{
-                  backgroundColor: `${props.color}`,
-                  borderRadius: `${props.borderRadius}px`,
-                }}
-              >
-                See my work
-              </button>
-            </motion.div>
+          
             {/* services */}
             <motion.div
-              variants={fadeIn("left", 0.5)}
+              variants={fadeIn("right", 0.5)}
               initial="hidden"
               whileInView={"show"}
               viewport={{ once: false, amount: 0.3 }}
               className="flex-1"
             >
-              <div className="">
+              
                 {services.map((service, index) => {
                   const { name, description, link } = service;
                   return (
@@ -102,7 +80,31 @@ const Services = (props) => {
                     </div>
                   );
                 })}
-              </div>
+              
+            </motion.div>
+            <motion.div
+              variants={fadeIn("left", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.3 }}
+              className="flex-1 lg:bg-services btn lg:bg-bottom bg-no-repeat mix-blend-normal rounded-xl p-10 mr-5 mb-12 lg:mb-0"
+            >
+              
+              <h2 className="h2 mb-6" style={{ color: `${props.color}` }}>
+                What I Do.{" "}
+              </h2>
+              <h3 className="h3 max-w-[455px] mb-6">
+                I'm a Frontend Developer with over 1 year of experience.
+              </h3>
+              <button
+                className="btn btn-sm z-50"
+                style={{
+                  backgroundColor: `${props.color}`,
+                  borderRadius: `${props.borderRadius}px`,
+                }}
+              >
+                See my work
+              </button>
             </motion.div>
           </div>
         </div>
